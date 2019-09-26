99a23aae9ae12adbcd88252cf59c1e1666db93be
Abel <abelberhane011@gmail.com>
2019-09-26 16:14:06 +0300

    adds input forms to contact us section and aligns it

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,11 @@ $(document).ready(function () {
         $(".img3-class").toggle();
     });
 });
+$(document).ready(function() {
+    $("form.inputs").submit(function(event) {
+    event.preventDefault();
+    var name = $(".name").val();
+    var email = $(".email").val();
+    alert(name + " we have received your message. Thank you for reaching out to us." );
+    });
+    });
