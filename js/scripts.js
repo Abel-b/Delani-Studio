878f33f02b5f4d411d2134fc23f84b0e989c925c
Abel <abelberhane011@gmail.com>
2019-09-26 12:17:32 +0300

    adds clickable jquery effects

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $(".clickable").click(function () {
+        $(".design").toggle();
+        $(".img-class").toggle();
+    });
+});
+
+$(document).ready(function() {
+    $(".clickable2").click(function() {
+      $(".development").toggle();
+      $(".img2-class").toggle();
+    });
+  });
+  $(document).ready(function() {
+      $(".clickable3").click(function() {
+        $(".product").toggle();
+        $(".img3-class").toggle();
+      });
+    });
