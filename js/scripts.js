a74870ea756f63dc0517dcaf2e9e3fb4ba80449d
Abel <abelberhane011@gmail.com>
2019-09-26 12:19:34 +0300

    Corrects the indentation of js file

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,15 @@ $(document).ready(function () {
     });
 });
 
-$(document).ready(function() {
-    $(".clickable2").click(function() {
-      $(".development").toggle();
-      $(".img2-class").toggle();
+$(document).ready(function () {
+    $(".clickable2").click(function () {
+        $(".development").toggle();
+        $(".img2-class").toggle();
     });
-  });
-  $(document).ready(function() {
-      $(".clickable3").click(function() {
+});
+$(document).ready(function () {
+    $(".clickable3").click(function () {
         $(".product").toggle();
         $(".img3-class").toggle();
-      });
     });
+});
