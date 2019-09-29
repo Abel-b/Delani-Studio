74762b289cf51d11752bc89b6fe7f769fb7e89ad
Abel <abelberhane011@gmail.com>
2019-09-29 16:43:49 +0300

    positions scripts link

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,18 @@ $(document).ready(function () {
         $(".design").toggle();
         $(".img-class").toggle();
     });
-});
-
-$(document).ready(function () {
     $(".clickable2").click(function () {
         $(".development").toggle();
         $(".img2-class").toggle();
     });
-});
-$(document).ready(function () {
     $(".clickable3").click(function () {
         $(".product").toggle();
         $(".img3-class").toggle();
     });
-});
-$(document).ready(function() {
     $("form.inputs").submit(function(event) {
     event.preventDefault();
     var name = $(".name").val();
     var email = $(".email").val();
     alert(name + " we have received your message. Thank you for reaching out to us." );
     });
-})
+});
